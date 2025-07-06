c814cbb50cb9ec9034f48e5056e61e9d1b021eca
Mohammadreza Mohammadi <mrezamhmdi97@gmail.com>
2025-07-07 00:33:27 +0200

    modified voice speed

--- app/services/audio_service.py
+++ app/services/audio_service.py
@@ -5,7 +5,7 @@ class AudioService:
     @staticmethod
     def create_audio(text: str, lang: str = 'nl') -> io.BytesIO:
         try:
-            tts = gTTS(text, lang=lang)
+            tts = gTTS(text, lang=lang, slow=True)
             buf = io.BytesIO()
             tts.write_to_fp(buf)
             buf.seek(0)
